011091f8935b45b2f64b68c16372d356d0bc20af
阳明 <yangming@tongbanjie.com>
2016-11-14 14:50:10 +0800

    init package

---
+++ src/main/java/io/sunyi/testbigdata/hadoop/TestMapReduce.java
@@ -0,0 +1,12 @@
+package io.sunyi.testbigdata.hadoop;
+
+/**
+ * @author sunyi
+ */
+public class TestMapReduce {
+
+	public static void main(String args[]) {
+		System.out.println("map reduce test");
+	}
+
+}
